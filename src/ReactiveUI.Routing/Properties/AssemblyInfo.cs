c587d3cad53504fbe8b60f299b2ae647f61ddb9f
Kallyn Gowdy <kallyngowdy@gmail.com>
2016-08-09 15:16:04 -0400

    Add ReactiveUI.Routing nuspec

--- src/ReactiveUI.Routing/Properties/AssemblyInfo.cs
+++ src/ReactiveUI.Routing/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ReactiveUI.Routing")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A routing library for ReactiveUI")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ReactiveUI.Routing")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
