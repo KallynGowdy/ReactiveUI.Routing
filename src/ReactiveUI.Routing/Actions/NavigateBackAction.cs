74db96ad760aac3888dce4fb8e5a5cc7dd6663d5
Kallyn Gowdy <kallyngowdy@gmail.com>
2016-08-10 20:23:25 -0400

    Implement Tests for RouterActions

--- src/ReactiveUI.Routing/Actions/NavigateBackAction.cs
+++ src/ReactiveUI.Routing/Actions/NavigateBackAction.cs
@@ -9,7 +9,34 @@ namespace ReactiveUI.Routing.Actions
     /// <summary>
     /// Defines a <see cref="IRouterAction"/> that specifies that the router should navigate backwards.
     /// </summary>
-    public class NavigateBackAction : IRouterAction
+    public class NavigateBackAction : IRouterAction, IEquatable<NavigateBackAction>
     {
+        public bool Equals(NavigateBackAction other)
+        {
+            return true;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((NavigateBackAction) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return 1;
+        }
+
+        public static bool operator ==(NavigateBackAction left, NavigateBackAction right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(NavigateBackAction left, NavigateBackAction right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
--- src/ReactiveUI.Routing/Actions/ShowViewModelAction.cs
+++ src/ReactiveUI.Routing/Actions/ShowViewModelAction.cs
@@ -9,13 +9,40 @@ namespace ReactiveUI.Routing.Actions
     /// <summary>
     /// Defines an action that instructs the router to show a view model with parameters.
     /// </summary>
-    public sealed class ShowViewModelAction : IRouterAction
+    public sealed class ShowViewModelAction : IRouterAction, IEquatable<ShowViewModelAction>
     {
         /// <summary>
         /// The parameters that should be used to activate the view model.
         /// </summary>
         public ActivationParams ActivationParams { get; set; }
 
+        public bool Equals(ShowViewModelAction other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Equals(ActivationParams, other.ActivationParams);
+        }
 
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj is ShowViewModelAction && Equals((ShowViewModelAction) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return ActivationParams?.GetHashCode() ?? 0;
+        }
+
+        public static bool operator ==(ShowViewModelAction left, ShowViewModelAction right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(ShowViewModelAction left, ShowViewModelAction right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
