2efccf4498546e55d16f5160dad1789f75beaed0
Kallyn Gowdy <kallyngowdy@gmail.com>
2017-08-18 22:47:41 -0400

    Add core project and related test project

---
+++ src/ReactiveUI.Routing.Core.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ReactiveUI.Routing.Core.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ src/ReactiveUI.Routing.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ReactiveUI.Routing.Core
+{
+    public class Class1
+    {
+    }
+}
